30b74f6f04ada1d7877f3227b42f5f96dfc61053
lukuankuan <lukuankuan@zhuanzhuan.com>
2023-08-11 15:26:52 +0800

    feat: 提交备案信息

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -2,18 +2,24 @@
   <div class="bottom">
     <div class="bottom-container">
       <div class="bottom-container-left">
-        <div class="bottom-container-text">北京市朝阳区益得工匠职业技能培训学校</div>
         <div class="bottom-container-text">
-          <a target="_blank" href="https://beian.miit.gov.cn/">
+          <a target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010502053244">
             <img class="bottom-container-text_img" src="../assets/icon.png"/>
             <span style="color:#939393;">
               京公网安备 11010502053244号
             </span>
           </a >
         </div>
+        <div class="bottom-container-text">北京市朝阳区益得工匠职业技能培训学校</div>
       </div>
       <div class="bottom-container-right">
-        <div class="bottom-container-text"></div>
+        <div class="bottom-container-text">
+          <a target="_blank" href="https://beian.miit.gov.cn/">
+            <span style="color:#939393;">
+              京ICP备2023020268号
+            </span>
+          </a >
+        </div>
         <div class="bottom-container-text">&copy;Copyright 2022-至今 www.cyydgj.com All Right Reserved</div>
       </div>
     </div>
